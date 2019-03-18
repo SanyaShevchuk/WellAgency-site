afe509e7680354d612c057a31735371b634c8ead
sheva <shevchuksanya983@gmail.com>
2019-03-18 13:17:26 +0200

    adaptive 480px w/o background

--- functionality.js
+++ functionality.js
@@ -1,17 +1,31 @@
 var menuIcon = document.getElementsByClassName('menu_icon');
 
 for(let i = 0 ; i < menuIcon.length; i++){
-  menuIcon[i].onclick = function(){
-    let menuPage = document.getElementById('menu_page');
-    menuPage.style.display = "block";
-    menuPage.style.top = (100*(i)) + "%";
+    menuIcon[i].onclick = function(){
+      if(window.matchMedia("(max-width: 480px)").matches){
+        let menuPage = document.getElementById('mob-menu-page');
+        menuPage.style.display = "block";
+        menuPage.style.top = (100*(i)) + "%";    
+      } else{
+        let menuPage = document.getElementById('menu_page');
+        menuPage.style.display = "block";
+        menuPage.style.top = (100*(i)) + "%";    
+      }
+    }
+}
+
+var closeButton = document.getElementsByClassName('close');
+for(let i=0; i < closeButton.length; i++){
+  closeButton[i].onclick = function(){
+    if(window.matchMedia('(max-width: 480px)').matches){
+      document.getElementById('mob-menu-page').style.display = 'none';
+    } else document.getElementById('menu_page').style.display = 'none'; 
   }
 }
 
-document.getElementById('close').onclick = function(){
-  document.getElementById('menu_page').style.display = 'none';
-}
 
 function closeMenu(){
-  document.getElementById('menu_page').style.display = "none";
+  if(window.matchMedia('(max-width: 480px)').matches){
+    document.getElementById('mob-menu-page').style.display = 'none';
+  } else document.getElementById('menu_page').style.display = "none";
 }
