bd13ccdf1564ed6ed0c425f97e0a5a600a46d442
sheva <shevchuksanya983@gmail.com>
2019-03-19 09:32:51 +0200

    adaptive 480px is not really done

--- functionality.js
+++ functionality.js
@@ -2,7 +2,8 @@ var menuIcon = document.getElementsByClassName('menu_icon');
 
 for(let i = 0 ; i < menuIcon.length; i++){
     menuIcon[i].onclick = function(){
-      if(window.matchMedia("(max-width: 480px)").matches){
+      if(window.matchMedia("(max-width: 480px)").matches
+      || window.matchMedia("(max-width: 767px)").matches){
         let menuPage = document.getElementById('mob-menu-page');
         menuPage.style.display = "block";
         menuPage.style.top = (100*(i)) + "%";    
@@ -17,7 +18,8 @@ for(let i = 0 ; i < menuIcon.length; i++){
 var closeButton = document.getElementsByClassName('close');
 for(let i=0; i < closeButton.length; i++){
   closeButton[i].onclick = function(){
-    if(window.matchMedia('(max-width: 480px)').matches){
+    if(window.matchMedia('(max-width: 480px)').matches
+    || window.matchMedia("(max-width: 767px)").matches){
       document.getElementById('mob-menu-page').style.display = 'none';
     } else document.getElementById('menu_page').style.display = 'none'; 
   }
@@ -25,7 +27,8 @@ for(let i=0; i < closeButton.length; i++){
 
 
 function closeMenu(){
-  if(window.matchMedia('(max-width: 480px)').matches){
+  if(window.matchMedia('(max-width: 480px)').matches
+    || window.matchMedia("(max-width: 767px)").matches){
     document.getElementById('mob-menu-page').style.display = 'none';
   } else document.getElementById('menu_page').style.display = "none";
 }
