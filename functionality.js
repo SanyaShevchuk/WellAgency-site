4ec43a2e12d1ed478883e850d55b3cd23a419549
sheva <shevchuksanya983@gmail.com>
2019-03-20 11:15:10 +0200

    adaptive design for portrait orientation is done

--- functionality.js
+++ functionality.js
@@ -2,8 +2,9 @@ var menuIcon = document.getElementsByClassName('menu_icon');
 
 for(let i = 0 ; i < menuIcon.length; i++){
     menuIcon[i].onclick = function(){
-      if(window.matchMedia("(max-width: 600px)").matches
-      || window.matchMedia("(max-width: 1100px)").matches){
+      if((window.matchMedia("(max-width: 600px)").matches 
+      || window.matchMedia("(max-width: 1100px)").matches)
+      && window.matchMedia('(orientation: portrait)').matches){
         let menuPage = document.getElementById('mob-menu-page');
         menuPage.style.display = "block";
         menuPage.style.top = (100*(i)) + "%";    
@@ -18,8 +19,9 @@ for(let i = 0 ; i < menuIcon.length; i++){
 var closeButton = document.getElementsByClassName('close');
 for(let i=0; i < closeButton.length; i++){
   closeButton[i].onclick = function(){
-    if(window.matchMedia('(max-width: 600px)').matches
-    || window.matchMedia("(max-width: 1100px)").matches){
+    if((window.matchMedia('(max-width: 600px)').matches 
+    || window.matchMedia("(max-width: 1100px)").matches)
+    && window.matchMedia('(orientation: portrait)').matches){
       document.getElementById('mob-menu-page').style.display = 'none';
     } else document.getElementById('menu_page').style.display = 'none'; 
   }
@@ -27,8 +29,9 @@ for(let i=0; i < closeButton.length; i++){
 
 
 function closeMenu(){
-  if(window.matchMedia('(max-width: 600px)').matches
-    || window.matchMedia("(max-width: 1100px)").matches){
+  if((window.matchMedia('(max-width: 600px)').matches
+    || window.matchMedia("(max-width: 1100px)").matches)
+    && window.matchMedia('(orientation: portrait)').matches){
     document.getElementById('mob-menu-page').style.display = 'none';
   } else document.getElementById('menu_page').style.display = "none";
 }
